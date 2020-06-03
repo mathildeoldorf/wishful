877886c00b361eec1dbaf5d8c60b9be6086af25c
Mathilde Runge <maro@html24.net>
2020-06-03 21:29:31 +0200

    again

---
+++ server/public/precache-manifest.92b7bd56b458d6591c0b1af0a73e494a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2848adccf14e64feb3132a892174fd40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3f18990ba995e6e3a0",
+    "url": "/static/css/main.d49bbe9c.chunk.css"
+  },
+  {
+    "revision": "1c79af6b45efcc6853bf",
+    "url": "/static/js/2.23dcfc55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.23dcfc55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3f18990ba995e6e3a0",
+    "url": "/static/js/main.20aaa9b1.chunk.js"
+  },
+  {
+    "revision": "7a2dbf035bb889cae4ed",
+    "url": "/static/js/runtime-main.faff3469.js"
+  }
+]);
